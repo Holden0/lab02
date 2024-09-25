bdba53a5224a0dea6ab49f03da0669738f9d0137
holdeno0 <hbudiman1@my.bcit.ca>
2024-09-24 20:31:22 -0700

    adding content for lab02

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+    return (
+        <h1>Best-Selling Music Artists</h1>
+    )
+}
+
+export default Header;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { artists } from './best-selling-music-artists';
+import React from 'react';
+import Header from '/components/Header'
+import ArtistDetails from '/components/ArtistDetails';
 
 function App() {
-
   return (
     <div className="app">
-      
+          <Header/>
+            <div className="artist-list">
+                {artists.map((artist, index) => (
+                    <ArtistDetails key={index} artist={artist} />
+                ))}
+            </div>
     </div>
   );
 }
